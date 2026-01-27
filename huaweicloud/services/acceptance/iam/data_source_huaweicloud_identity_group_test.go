17e75310d729add6286ec75e95f71ede3e02d653
greyworld <huawei@ecs-yanghongyu>
2026-01-27 16:24:38 +0800

    test(iam): optimize the acceptance case design for group datasource

--- huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_group_test.go
+++ huaweicloud/services/acceptance/iam/data_source_huaweicloud_identity_group_test.go
@@ -9,10 +9,14 @@ import (
 	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/services/acceptance"
 )
 
-func TestAccIdentityGroupDataSource_basic(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_group.test"
-	rName := acceptance.RandomAccResourceName()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
+func TestAccDataSourceGroup_basic(t *testing.T) {
+	var (
+		rName    = acceptance.RandomAccResourceName()
+		password = acceptance.RandomPassword()
+
+		dcName = "data.huaweicloud_identity_group.test"
+		dc     = acceptance.InitDataSourceCheck(dcName)
+	)
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
@@ -22,77 +26,61 @@ func TestAccIdentityGroupDataSource_basic(t *testing.T) {
 		ProviderFactories: acceptance.TestAccProviderFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityGroupDataSource_by_name(rName),
+				Config: testAccDataSourceGroup_basic_step1(rName),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
-					resource.TestCheckResourceAttr(dataSourceName, "users.#", "0"),
+					resource.TestCheckResourceAttr(dcName, "name", rName),
+					resource.TestCheckResourceAttr(dcName, "users.#", "0"),
 				),
 			},
-		},
-	})
-}
-
-func TestAccIdentityGroupDataSource_with_user(t *testing.T) {
-	dataSourceName := "data.huaweicloud_identity_group.test"
-	rName := acceptance.RandomAccResourceName()
-	password := acceptance.RandomPassword()
-	dc := acceptance.InitDataSourceCheck(dataSourceName)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck: func() {
-			acceptance.TestAccPreCheck(t)
-			acceptance.TestAccPreCheckAdminOnly(t)
-		},
-		ProviderFactories: acceptance.TestAccProviderFactories,
-		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityGroupDataSource_with_user(rName, password),
+				Config: testAccDataSourceGroup_basic_step2(rName, password),
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceName, "name", rName),
-					resource.TestCheckResourceAttr(dataSourceName, "users.#", "2"),
-					resource.TestCheckResourceAttrSet(dataSourceName, "users.0.id"),
+					resource.TestCheckResourceAttr(dcName, "name", rName),
+					resource.TestCheckResourceAttr(dcName, "users.#", "2"),
+					resource.TestCheckResourceAttrSet(dcName, "users.0.id"),
 				),
 			},
 		},
 	})
 }
 
-func testAccIdentityGroupDataSource_by_name(rName string) string {
-	return fmt.Sprintf(`
-resource "huaweicloud_identity_group" "test" {
-  name        = "%s"
-  description = "An ACC test group"
-}
-
-data "huaweicloud_identity_group" "test" {
-  name = huaweicloud_identity_group.test.name
-  
-  depends_on = [
-    huaweicloud_identity_group.test
-  ]
-}
-`, rName)
-}
-
-func testAccIdentityGroupDataSource_with_user(rName, password string) string {
+func testAccDataSourceGroup_base(rName string) string {
 	return fmt.Sprintf(`
 resource "huaweicloud_identity_group" "test" {
   name        = "%[1]s"
   description = "An ACC test group"
 }
+`, rName)
+}
+
+func testAccDataSourceGroup_basic_step1(rName string) string {
+	return fmt.Sprintf(`
+%[1]s
+
+data "huaweicloud_identity_group" "test" {
+  name = huaweicloud_identity_group.test.name
+}
+`, testAccDataSourceGroup_base(rName))
+}
+
+func testAccDataSourceGroup_basic_step2(rName, password string) string {
+	return fmt.Sprintf(`
+%[1]s
 
 resource "huaweicloud_identity_user" "test" {
   count    = 2
-  name     = "%[1]s-${count.index}"
-  password = "%[2]s"
+  name     = "%[2]s-${count.index}"
+  password = "%[3]s"
   enabled  = true
 }
 
 resource "huaweicloud_identity_group_membership" "test" {
   group = huaweicloud_identity_group.test.id
-  users = huaweicloud_identity_user.test.*.id
+  users = [
+    for user in huaweicloud_identity_user.test : user.id
+  ]
 }
 
 data "huaweicloud_identity_group" "test" {
@@ -102,5 +90,5 @@ data "huaweicloud_identity_group" "test" {
     huaweicloud_identity_group_membership.test
   ]
 }
-`, rName, password)
+`, testAccDataSourceGroup_base(rName), rName, password)
 }
